734ab794d428e3ef82eade8d00d20c25248ec57c
johnny vang <johnnyjvang@gmail.com>
2025-05-27 23:05:39 -0700

    variable basics

---
+++ variables_basics.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main(){
+    // integer (whole number)
+    int x; //declaration
+    x = 5;
+
+    // double (number including decimal)
+    double price = 10.99;
+    double gpa = 2.56;
+
+    std::cout << x << std::endl;
+    std::cout << price << " and " << gpa << std::endl;
+
+    // single chracter 
+    char grade = 'A';
+    char initial = 'D';
+
+    std::cout << "student got:" << grade << std::endl;
+    std::cout << "student first initial is: " << initial << std::endl;
+
+
+
+    return 0;
+}
