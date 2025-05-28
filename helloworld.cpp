ec9ebb33fcb32264202e7d88c51c677583142fe3
johnny vang <johnnyjvang@gmail.com>
2025-05-27 19:35:19 -0700

    first commit

---
+++ helloworld.cpp
@@ -0,0 +1,14 @@
+// iostream = headerfile contains lots of userful things
+#include <iostream>
+
+int main() {
+    // standard cout = character output 
+    // << = move into 
+    // std = namespace, contains varaibles like cout 
+    std::cout << "HELLO WORLD" << std::endl;
+    std::cout << "new world" << "\n";
+
+    // return 0 = no problem in the program 
+    // return 1 = some problem in the program 
+    return 0;
+}
